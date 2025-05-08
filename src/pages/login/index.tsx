f0fcc3efb19f188bee495f4638ccfda952f5caa7
Guilherme Dorigon <guidbovo@gmail.com>
2025-05-07 21:32:02 -0300

    Criando página de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,53 @@
 import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { FormEvent, useState } from "react";
+
 
 
 export function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+
+
+        console.log({
+            email: email,
+            password: password
+        })
+    }
+
+
     return (
         <h1 className="flex w-full h-screen items-center justify-center flex-col">
             <Link to="/">
-                <h1 className="mt-11 text-white mb-7 font-bold text-5xl">Dev
-                    <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span>
+                <h1 className="mt-11 text-white mb-7 font-bold text-5xl">Dori
+                    <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Links</span>
                 </h1>
             </Link>
+
+            <form onSubmit={handleSubmit} className="w-full max-w-xl flex flex-col">
+                <Input
+                    placeholder="Email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+
+                <button type="submit" className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white">
+                    Acessar
+                </button>
+            </form>
         </h1>
     )
 }
