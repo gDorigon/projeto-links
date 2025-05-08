f9de840eefacf4a303fb866caf209621218bd919
Guilherme Dorigon <guidbovo@gmail.com>
2025-05-08 20:41:48 -0300

    Criando metodo de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,22 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Input } from "../../components/Input";
 import { FormEvent, useState } from "react";
 
 import { auth } from "../../services/firebaseConnection";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export function Login() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault();
 
+        if (email === '' || password === '') {
+            alert("Preencha os campos vazios")
+        }
 
-        console.log({
-            email: email,
-            password: password
-        })
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                navigate("/admin", { replace: true })
+
+            })
+
+            .catch(() => {
+
+            })
     }
 
 
