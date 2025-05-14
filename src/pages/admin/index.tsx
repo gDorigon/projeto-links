4d178b09dd32857e1d9d3862214ad092c5148bca
Guilherme Dorigon <guidbovo@gmail.com>
2025-05-14 20:49:32 -0300

    Estilizando header

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from "../../components/header";
 
 
 export function Admin() {
     return (
-        <h1>
-            Pagina Adm
-        </h1>
+        <div className="flex items-center flex-col min-h-screen pb-7 px-2">
+            <Header />
+        </div>
     )
 }
